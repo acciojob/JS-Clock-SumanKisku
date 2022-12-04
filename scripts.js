af0fc5356e1905f9e88af98bec0ec13586ba4c1b
Suman Kisku <sumankisku1@gmail>
2022-12-05 01:09:41 +0530

    done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 //your code here
+    // getting the clock hand elements
+const hhand = document.querySelector(".hour-hand");
+const mhand = document.querySelector(".min-hand");
+const shand = document.querySelector(".second-hand");
+
+setInterval(() =>{
+    let d = new Date();
+    let hour = d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+
+    let hourdeg = 30 * hour + minutes / 2 + 90;
+    let mindeg = 6 * minutes + 90;
+    let secondsdeg = 6 * seconds + 90;
+    hhand.style.transform = `rotate(${hourdeg}deg)`;
+    mhand.style.transform = `rotate(${mindeg}deg)`;
+    shand.style.transform = `rotate(${secondsdeg}deg)`;
+}, 1000)
+
